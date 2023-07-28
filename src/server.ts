ae0c655a894fc2e670a999a710eb78b5cea1e6ed
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-29 01:49:24 +0900

    rm

--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,6 @@ app.use(express.json());
 app.post("/todos", (req: Request, res: Response) =>
   todoController.add(req, res)
 );
-// app.put("/todos/:id", (req: Request, res: Response) =>
-//   todoController.toggle(req, res)
-// );
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
